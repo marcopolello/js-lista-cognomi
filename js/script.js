2088c3060d974b6cd472b6cf7e2dd48fd4c48378
marcopolello <marco-pol@hotmail.it>
2020-10-23 14:27:08 +0200

    bottone mi ordina i nomi alfabeticamente

--- js/script.js
+++ js/script.js
@@ -5,8 +5,9 @@
 // scrivi anche la posizione “umana” della lista in cui il nuovo utente si trova
 
 // Lista Cognomi
-var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
-console.log(cognomi);
+var cognomi = ["bianchi", "rossi", "duzioni", "balsano", "verdi"];
+// console.log(cognomi);
+document.getElementById('lista').innerHTML = cognomi
 // Chiedere all’utente il cognome
 var utente = prompt("scrivi il tuo cognome");
 // inserirlo in un array con gli altri cognomi
@@ -15,13 +16,16 @@ console.log(cognomi);
 
 // stampa la lista ordinata alfabeticamente
 var ordina = document.getElementById('button');
-ordina.addEventLIstener('click', function()
+ordina.addEventListener('click', function ()
 {
+  cognomi.sort();
   var i = 0;
   while (i < cognomi.length)
   {
-    cognomi.sort();
+    // var cognomi = cognomi + "<li>" + cognomi[i] + "</li>"
     i++;
   }
   console.log(cognomi);
+
+  document.getElementById('stampa').innerHTML = cognomi
 });
